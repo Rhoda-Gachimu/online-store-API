5b518b63daca6cba4142569dbddb41696b94c2cd
Rhoda Gachimu <gachimurhoda@gmail.com>
2024-05-30 13:50:45 +0300

    map urls to views

--- instastore/views.py
+++ instastore/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse  
 
+# Http is a module, not a class.(a package)
+# HttpResponse is a class, so we need to instantiate it.
 # Create your views here.
+
+def say_hello(request):
+    return HttpResponse('Hello World!')
+
+
+# map this request to a URL so that when i get a request to this URL,this function will be called
